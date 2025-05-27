b6e86eb161cff8bc56382d4147364ee7953ddf32
dhiraj <developer@change-networks.com>
2025-05-27 18:20:49 +0530

    minor chages

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            COMMANDO NETWORKS
+            SIEM NETWORKS
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
