d2eba4a3633cbacb81df46a8a17b4a5212376792
dhiraj <developer@change-networks.com>
2025-05-28 18:25:40 +0530

    minor chnages'

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  eslint:{
-    ignoreDuringBuilds:true
-  },
-  images:{
-    unoptimized:true
-  },
-  typescript:{
-    ignoreBuildErrors:true
-  }
 };
 
 export default nextConfig;
