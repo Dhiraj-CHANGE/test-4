2ce5021321133c8ae24d8608acaec47b0cfcb5d7
dhiraj <developer@change-networks.com>
2025-05-28 19:42:41 +0530

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            CHANGE Networks 
+            CHANGE Networks Dhiraj Chaudhari 
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
