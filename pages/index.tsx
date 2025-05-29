570856dab4182ef534508be7627e8260b3acca56
dhiraj <developer@change-networks.com>
2025-05-29 13:25:23 +0530

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            COMMANDO 
+            COMMANDO  Netowkrws
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
