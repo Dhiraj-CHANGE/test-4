efed3a50c8c182a0d6619eb36734c67797f7e97c
dhiraj <developer@change-networks.com>
2025-05-29 21:18:19 +0530

    minor chages

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            CHANGE Netowrks COMMANDO
+            CHANGE Netowrks Dhiraj 
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
