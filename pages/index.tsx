8aed170c25bd3a5128fcb0e92d2d82d8731935b1
dhiraj <developer@change-networks.com>
2025-05-29 16:07:17 +0530

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            COMMANDO CHANGE Networks
+            COMMANDO  Networks
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
