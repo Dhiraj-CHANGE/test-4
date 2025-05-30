01c4bf71d1b54dccbf8cc173c4bca4ae6b884f8d
dhiraj <developer@change-networks.com>
2025-05-30 13:43:55 +0530

    Minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            DEV Branch
+            Hello world
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
