3853d0056aa36dfbbf077a32ac0160d94b6cff61
dhiraj <developer@change-networks.com>
2025-05-30 20:06:23 +0530

    update yml file

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            VERCEL CICD 4
+            VERCEL CICD 5
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
