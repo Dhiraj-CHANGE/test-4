07717229652c262367297cbe2ff8bfd33b0eed0a
Dhiraj-CHANGE <dhiraj.chaudhari@change-networks.com>
2025-05-31 12:21:31 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            VERCEL CICD dev-5
+            VERCEL CICD dev-6
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
